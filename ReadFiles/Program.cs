af5955540617f281e453a9a9639f37f98a933936
rhartcch <rhart@clearcosthealth.com>
2022-11-28 12:43:46 -0800

    Testing
    Testing

--- ReadFiles/Program.cs
+++ ReadFiles/Program.cs
@@ -14,19 +14,16 @@ foreach (var file in files)
     string jsonString = r.ReadToEnd();
     var contents  = JsonSerializer.Deserialize<Payload>(jsonString);
     string connectionString = "";
-    var sql = @"Select top 2 IsNull(a.Line1, ''), IsNull(a.Line2, ''), IsNull(a.Line3, ''), IsNull(a.CityName, ''), IsNull(a.UspsCode, ''), 
-                        IsNull(a.ZipPostalCode, ''), a.Coordinates.Lat, a.Coordinates.Long
-                        From NpiAddress n 
-                        inner join vwAddress a on n.AddressId = a.AddressId 
-						Where n.Npi = @Npi and n.employerPayerID = @employerPayerID
-                        order by n.AddressPriority";
-    using (SqlConnection conn = new SqlConnection(connectionString)) ;
+    var sql = @"Select *";
+    using (SqlConnection connection = new SqlConnection(connectionString)) 
     {
-        using (SqlCommand cmd = new SqlCommand(sql, conn))
+        using (SqlCommand command = connection.CreateCommand())
         {
-            cmd.Parameters.AddWithValue("@Npi", Npi);
-            cmd.Parameters.AddWithValue("@employerPayerID", employerPayerID);
-            using (SqlDataReader rdr = await cmd.ExecuteReaderUncommitedAsync().ConfigureAwait(false))
+            command.CommandText = sql;
+            //cmd.Parameters.AddWithValue("@Npi", Npi);
+            command.ExecuteNonQuery();
+
+            using (SqlDataReader rdr = command.ExecuteReader())
             {
                 while (await rdr.ReadAsync().ConfigureAwait(false))
                 {
